cb8875cc32269888300ca25ee16f76d8b212a43d
caduh-uh <carlos.eduardo1801@hotmai.com>
2024-03-13 16:49:16 -0300

    adicionado API de cliente

---
+++ src/main/java/br/com/fiap/hackgrupo01/controller/ClienteController.java
@@ -0,0 +1,56 @@
+package br.com.fiap.hackgrupo01.controller;
+
+import br.com.fiap.hackgrupo01.model.dto.cliente.ClienteDTO;
+import br.com.fiap.hackgrupo01.service.ClienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/cliente")
+@Tag(name = "Gestão de clientes", description = "Serviço de gerenciamento da clientes.")
+public class ClienteController {
+
+    @Autowired
+    private ClienteService service;
+
+    @GetMapping
+    @Operation(summary = "Buscar todos os clientes")
+    ResponseEntity<List<ClienteDTO>> findAll(){
+        var response = service.findAll();
+
+        return response.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Buscar clientes por id")
+    ResponseEntity<ClienteDTO> findById(@PathVariable(name = "id") Long id){
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @PostMapping
+    @Operation(summary = "Cadastro de Cliente")
+    ResponseEntity<ClienteDTO> create(@RequestBody ClienteDTO dto){
+        return ResponseEntity.ok(service.create(dto));
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Edição de Cliente")
+    ResponseEntity<ClienteDTO> update(@PathVariable(name = "id") Long id, @RequestBody ClienteDTO dto){
+        return ResponseEntity.ok(service.update(dto, id));
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Exclusão de Cliente")
+    ResponseEntity<?> delete(@PathVariable(name = "id") Long id){
+        service.delete(id);
+        return ResponseEntity.status(HttpStatusCode.valueOf(200)).build();
+    }
+}
---
+++ src/main/java/br/com/fiap/hackgrupo01/model/cliente/Cliente.java
@@ -0,0 +1,29 @@
+package br.com.fiap.hackgrupo01.model.cliente;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String paisOrigem;
+
+    @Column(unique = true)
+    private String cpf;
+    private String passaporte;
+    private String nomeCompleto;
+    private String dataNascimento;
+    private String enderecoPaisOrigem;
+    private String telefone;
+    private String email;
+}
