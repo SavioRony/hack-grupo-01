cc8ccbf93c5622a7d42a6f1362b9915080609e11
caduh-uh <carlos.eduardo1801@hotmai.com>
2024-03-14 07:00:22 -0300

    ajustando API de cliente

---
+++ src/main/java/br/com/fiap/hackgrupo01/validations/Validator.java
@@ -0,0 +1,26 @@
+package br.com.fiap.hackgrupo01.validations;
+
+import br.com.fiap.hackgrupo01.exception.BadRequestException;
+import br.com.fiap.hackgrupo01.model.dto.cliente.ClienteDTO;
+
+public class Validator {
+
+    private Validator(){}
+
+    static final String PAIS_ORIGEM = "Brasil";
+
+    public static  void validateFields(ClienteDTO cliente){
+
+        StringBuilder sb = new StringBuilder();
+
+        if(PAIS_ORIGEM.equalsIgnoreCase(cliente.getPaisOrigem()) && cliente.getCpf() == null){
+            sb.append("Campo cpf Obrigatorio !!");
+        }else if(!PAIS_ORIGEM.equalsIgnoreCase(cliente.getPaisOrigem()) && cliente.getPassaporte() == null){
+            sb.append("Campo passaporte Obrigatorio");
+        }
+
+        if(!sb.isEmpty()){
+            throw new BadRequestException(sb.toString());
+        }
+    }
+}
